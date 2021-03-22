51958f22c217da246ba7f877cd44353a15d087d0
Koray Tug <koray.tug@virtudev.net>
2021-03-22 22:21:56 +0300

    add gradient background and font

--- src/App.js
+++ src/App.js
@@ -43,7 +43,8 @@ class App extends Component {
         const { monsters, searchField } = this.state
         const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
         return (
-            <div className="App"> 
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
                 <SearchBox
                     placeholder='search monsters'
                     handleChange={this.handleChange}
